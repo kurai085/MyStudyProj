2b1e779d51775b066757d09df6256a95a4d11f5b
sbyoon <sbyoon@mpole.com>
2016-12-09 17:54:26 +0900

    * 스칼라 학습. - 기본적으로 스칼라의 함수는 리턴타입이 정의 되어 있지 않으면, 맨 마지막실행줄의 결과를 리턴한다.

--- Scala/HellowWorld/src/main/scala/Example.scala
+++ Scala/HellowWorld/src/main/scala/Example.scala
@@ -2,5 +2,26 @@
   * Created by Bin on 2016-12-08.
   */
 object ExampleApp extends App {
-  println("Hello World!");
+  def abs(x: Double) = if(x < 0) -x else x
+
+  def sqrtIter(guess: Double, x: Double): Double=
+    if (isGoodEnough(guess, x))
+      guess
+    else sqrtIter(improve(guess,x),x)
+
+  def isGoodEnough(guess: Double, x: Double) =
+      abs(guess*guess-x) /x < 0.001
+
+  def improve(guess: Double, x: Double) =
+    (guess + x / guess) / 2
+
+  def sqrt(x: Double) = sqrtIter(1.0, x)
+
+ val rslt = sqrt(1)
+ val aa = isGoodEnough(1.0,1)
+// val rslt2 =  sqrt(4)
+
+ println(rslt);
+//  println(rslt2);
+//  println(sqrt(1e-6));
 }
